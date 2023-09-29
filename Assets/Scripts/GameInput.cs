de090e8c78d615a581ba3cc8c1d1a6d0f59d5935
javierfm <jadosfer@hotmail.com>
2023-09-29 13:29:30 -0300

    clean comments

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -12,22 +12,6 @@ public class GameInput : MonoBehaviour {
     public Vector2 GetMovementVectorNormalized() {
         Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
 
-        //if (Input.GetKey(KeyCode.W))  Todo esto queda reemplazado por la linea de arriba
-        //{
-        //    inputVector.y = +1;
-        //}
-        //if (Input.GetKey(KeyCode.S))
-        //{
-        //    inputVector.y = -1;
-        //}
-        //if (Input.GetKey(KeyCode.A))
-        //{
-        //    inputVector.x = -1;
-        //}
-        //if (Input.GetKey(KeyCode.D))
-        //{
-        //    inputVector.x = +1;
-        //}
         inputVector = inputVector.normalized; //normaliza para que en diagonal se mueva a la misma velocidad. Se puede hacer desde unity
         return inputVector;
     }
